e10381ee13e61a0274a324c84369867aac0e7db9
NIPUN TYAGI <116417772+Nipun539@users.noreply.github.com>
2023-08-13 00:04:38 -1200

    Update script.js

--- script.js
+++ script.js
@@ -40,11 +40,10 @@ $(document).ready(function () {
   
     var typed = new Typed(".typing", {
       strings: [
-        "Fullstack Developer",
-        "Software Developer",
-        "Python Developer",
-        "Founder",
-        "Author"
+        "Fullstack Web Developer",
+        "Graphic Designer",
+        "Content creator",
+        "UX/UI Designer",
       ],
       typeSpeed: 100,
       backSpeed: 60,
@@ -53,11 +52,10 @@ $(document).ready(function () {
   
     var typed = new Typed(".typing-2", {
       strings: [
-        "Fullstack Developer",
-        "Software Developer",
-        "Python Developer",
-        "Founder",
-        "Author"
+        "Fullstack Web Developer",
+        "Graphic Designer",
+        "Content creator",
+        "UX/UI Designer",
       ],
       typeSpeed: 100,
       backSpeed: 60,
